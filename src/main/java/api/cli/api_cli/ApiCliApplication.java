a5945597e0dc050a0458cf7207c37f56243a41ae
Cauan Lima <cauanvcdl@gmail.com>
2023-02-18 11:56:57 -0300

    Arquivos Iniciais do Spring Initializr

---
+++ src/main/java/api/cli/api_cli/ApiCliApplication.java
@@ -0,0 +1,13 @@
+package api.cli.api_cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCliApplication.class, args);
+	}
+
+}
